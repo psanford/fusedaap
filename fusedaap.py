4c1e9ef9eb84e883f97ff0f86c8f10b09204534c
Peter Sanford <peter@petersdanceparty.com>
2007-01-23 20:45:56 +0000

    Version bump to 0.3, added entry in changelog.
    git-svn-id: https://svn.code.sf.net/p/fusedaap/code/trunk@48 868f087a-b41f-0410-92e8-e087473d3cc9

--- fusedaap.py
+++ fusedaap.py
@@ -26,7 +26,7 @@
 
 __author__ = "Peter Sanford"
 __email__ = "peter dot sanford at wheaton dot edu"
-__version__ = "0.2.1"
+__version__ = "0.3"
 
 import os, stat, errno, sys, socket, time, signal
 import fuse
